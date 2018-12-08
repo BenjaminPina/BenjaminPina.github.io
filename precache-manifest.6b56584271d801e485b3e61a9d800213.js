96c60e0741a903fb4a7f28b7d08cb6d0d428941c
Benjamín Piña Altamirano <benjamin.pina@gmail.com>
2018-12-08 15:03:23 -0600

    Primer commit

---
+++ precache-manifest.6b56584271d801e485b3e61a9d800213.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a49b7257298aa1a1f2dc",
+    "url": "/static/js/main.a49b7257.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "a49b7257298aa1a1f2dc",
+    "url": "/static/css/main.edd281fb.chunk.css"
+  },
+  {
+    "revision": "2452d919b8294c9ffd061bc8c5b8b3bd",
+    "url": "/index.html"
+  }
+];
